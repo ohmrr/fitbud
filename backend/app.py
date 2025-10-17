960b51858cbe9c29ecaafc8ee376f9ec9ce019c6
ryanp4096 <ryanp4096@gmail.com>
2025-10-17 13:26:05 -0700

    post request

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("data", methods=['POST'])
+def data():
+    bytes = request.get_data()
+    
